c16571859c26fabd6b7c4bd844c4aa1119859f97
vartan <v.torossian@blackcover.fr>
2021-10-17 11:51:47 +0200

    feat: add migration & seeder

---
+++ app/Http/Controllers/MovieController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use \App\Models\Movie;
+use Illuminate\Http\Response;
+use \Illuminate\Support\Facades\Storage;
+use \Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Collection;
+
+class MovieController extends Controller
+{
+    /**
+     * @var string
+     */
+    protected $table = 'movies';
+
+    /**
+     * @var int
+     */
+    protected $primaryKey = 'id';
+}
